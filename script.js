40052c8245355d5ab8934f7c07400279852655a8
AkmalIhabSyauqi <akmalihab@gmail.com>
2024-04-23 09:38:01 +0700

    minor fix

--- script.js
+++ script.js
@@ -14,6 +14,6 @@ function showIndonesia(){
 
         document.getElementById('mode-text').innerText = 'indonesian'
         document.getElementById('language-icon').onclick = showEnglish()
-        document.getElementById('message').placeholder = '"Beri aku 1000 orang tua, niscaya akan kucabut semeru dari akarnya, beri aku 10 pemuda, niscaya akan kuguncang dunia." -Bung Karno'
+        document.getElementById('message').placeholder = '"Beri Aku 1000 Orang Tua, Niscaya akan Kucabut Semeru dari Akarnya, Beri Aku 10 Pemuda, Niscaya akan Kuguncang Dunia." -Bung Karno'
     });
 }
